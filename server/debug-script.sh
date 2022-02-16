41c0a0fd758ce6a365ac3ada0ef7e26c120f7041
Wirena <wirena27@gmail.com>
2022-02-17 02:17:45 +0300

    Add support for vs code debug tasks

---
+++ server/debug-script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+IMAGE_NAME="cfc-debug-image"
+CONTAINER_NAME="cfc-debug-cont"
+DOCKERFILE_PATH="debug.Dockerfile"
+
+function build() {
+    docker build -f ${DOCKERFILE_PATH} . --tag ${IMAGE_NAME}
+    return $?
+}
+
+function run() {
+    docker run -d -p2345:2345 -p8080:8080 --rm --name ${CONTAINER_NAME} ${IMAGE_NAME}
+    return $?
+}
+
+function stop() {
+    docker stop ${CONTAINER_NAME}
+    return $?
+}
+
+function all() {
+    build && run || echo "Something went wrong"
+}
+
+if [[ "$1" = "--build" || "$1" = "-b" ]]; then
+    build
+elif [[ "$1" = "--run" || "$1" = "-r" ]]; then
+    run
+elif [[ "$1" = "--stop" || "$1" = "-s" ]]; then
+    stop
+elif [[ "$1" = "--all" || "$1" = "-a" ]]; then
+    all
+else
+    echo "Use --wait N as last arg to give debugger N seconds to start before VS Code will try to attach"
+    echo "-b --build: build debug image"
+    echo "-r --run:   run new debug container, stop one beforehand if it's already running"
+    echo "-a --all:   do all of listed above"
+    echo "-s --stop:   stop debug container"
+    return 0
+fi
+
+WAIT_TIME=5
+if [[ $2 = "--wait" ]]; then
+    if [[ -n "$3" ]]; then
+        WAIT_TIME=$3
+    fi
+    sleep ${WAIT_TIME}
+fi
--- server/debug.Dockerfile
+++ server/debug.Dockerfile
@@ -6,6 +6,5 @@ RUN go install github.com/go-delve/delve/cmd/dlv@v1.8.1
 ENV PATH="/go/src/github.com/Wirena/clang-format-configurator-v2/third-party/clang-format:${PATH}"
 COPY . .
 
-
 CMD ["dlv", "debug", "./cmd/clang-format-configurator/", "--headless", \
     "--listen=:2345", "--api-version=2", "--log"]
