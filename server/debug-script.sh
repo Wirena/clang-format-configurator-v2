389d294b9a5221e08a462117d64a89a5a6e5896e
wirena <wirena@protonmail.com>
2022-02-22 16:49:35 +0300

    fix vscode tasks n launch

--- server/debug-script.sh
+++ server/debug-script.sh
@@ -9,8 +9,14 @@ function build() {
     return $?
 }
 
-function run() {
-    docker run -d -p2345:2345 -p8080:8080 --rm --name ${CONTAINER_NAME} ${IMAGE_NAME}
+function run-remote() {
+    docker run -p2345:2345 -p8080:8080 --rm --name ${CONTAINER_NAME} ${IMAGE_NAME}
+    return $?
+}
+
+function run-local() {
+    echo "Starting local debugger"
+    dlv debug ./cmd/clang-format-configurator/ --headless --listen=:2345 --api-version=2
     return $?
 }
 
@@ -20,25 +26,29 @@ function stop() {
 }
 
 function all() {
-    build && run || echo "Something went wrong"
+    echo "CatchPhrase"
+    build && run-remote || echo "Something went wrong"
 }
 
 WAIT_TIME=40
 
 if [[ "$1" = "--build" || "$1" = "-b" ]]; then
     build
-elif [[ "$1" = "--run" || "$1" = "-r" ]]; then
-    run
+elif [[ "$1" = "--run-remote" || "$1" = "-r" ]]; then
+    run-remote
+elif [[ "$1" = "--run-local" || "$1" = "-l" ]]; then
+    run-local
 elif [[ "$1" = "--stop" || "$1" = "-s" ]]; then
     stop
 elif [[ "$1" = "--all" || "$1" = "-a" ]]; then
     all
 else
     echo "Use --wait N as the last arg to give debugger N seconds to start before VS Code will try to attach(${WAIT_TIME} by default)"
-    echo "-b --build: build debug image"
-    echo "-r --run:   run new debug container, stop one beforehand if it's already running"
-    echo "-a --all:   do all of listed above"
-    echo "-s --stop:  stop debug container"
+    echo "-b --build:      build debug image"
+    echo "-r --run-remote: run new debug container, stop one beforehand if it's already running"
+    echo "-l --run-local:  build server and run debugger localy"
+    echo "-a --all:        do all of listed above"
+    echo "-s --stop:       stop debug container"
     return 0
 fi
 
