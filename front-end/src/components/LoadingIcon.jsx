b3e6ff23eeedc9a62d1b596865298ba91027d8f6
Wirena <62119056+Wirena@users.noreply.github.com>
2024-01-11 00:34:43 +0300

    Add format state indicator (#34)
    * Add loading icon
    
    * Loading icon class
    
    * Use LoadingIcon component
    
    * Fix loadingFailedIcon.svg name

---
+++ front-end/src/components/LoadingIcon.jsx
@@ -0,0 +1,35 @@
+import styles from "./LoadingIcon.module.css"
+import React from "react";
+
+class LoadingIcon extends React.Component {
+
+    constructor() {
+        super();
+        this.loadingState = "loading";
+        window.loadingIcon = this;
+    }
+
+    setLoadingState(state) {
+        this.loadingState = state
+        this.forceUpdate()
+    }
+
+    render() {
+        let image;
+        if (this.loadingState === "loading")
+            image = (<img className={styles.loading_icon} alt="loading" src="loadingIcon.gif" />);
+        else if (this.loadingState === "success")
+            image = (<img className={styles.loading_icon} alt="loading" src="./loadingFinishedIcon.svg" />);
+        else if (this.loadingState === "error")
+            image = (<img className={styles.loading_icon} alt="loading" src="./loadingFailedIcon.svg" />);
+        else {
+            console.error("LoadingIcon.jsx: Invalid state value - " + this.loadingState)
+            image = (<span></span>)
+        }
+        return (<span>{image}</span>)
+    }
+}
+
+
+
+export default LoadingIcon;
